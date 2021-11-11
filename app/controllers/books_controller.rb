3db78215b3796a3f995e32d675001aa220d27456
nemdull <dlanx@outlook.jp>
2021-11-11 05:26:22 +0000

    主要機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+		  redirect_to  book_path(@book.id)
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def index
@@ -22,6 +26,20 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to  book_path(@book.id)
+    else
+      @books = Book.all
+      render "edit"
+    end
   end
 
   private
