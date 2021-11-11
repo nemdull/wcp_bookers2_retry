ab0d058ea687ab02cb799ba97ffb66819ca97c05
nemdull <dlanx@outlook.jp>
2021-11-11 06:24:26 +0000

    主要機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get "homes/about" => "homes#about"
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 end
